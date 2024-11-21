e0144870d51b308e0a4bc653f3a37b59f689d7dc
MBURKU <rku@aarhus.dk>
2024-11-21 10:17:45 +0100

    Added logging to the kill_process_by_name function.

--- robot_framework/reset.py
+++ robot_framework/reset.py
@@ -27,7 +27,7 @@ def close_all(orchestrator_connection: OrchestratorConnection) -> None:
 def kill_all(orchestrator_connection: OrchestratorConnection) -> None:
     """Forcefully close all applications used by the robot."""
     orchestrator_connection.log_trace("Killing all applications.")
-    kill_process_by_name("TMTand.exe")
+    kill_process_by_name(orchestrator_connection, process_name="TMTand.exe")
 
 
 def kill_process_by_name(orchestrator_connection: OrchestratorConnection, process_name: str):
@@ -35,7 +35,7 @@ def kill_process_by_name(orchestrator_connection: OrchestratorConnection, proces
     orchestrator_connection.log_trace(f"Searching for process: {process_name}.")
     for proc in psutil.process_iter(['pid', 'name']):
         if proc.info['name'] == process_name:
-            orchestrator_connection.log_trace(f"Killing proc.info['name'].")
+            orchestrator_connection.log_trace(f"Killing {proc.info['name']}.")
             proc.kill()
             orchestrator_connection.log_trace(f"Killed process {proc.info['name']} with PID {proc.info['pid']}")
 
